0af0c3f07031a8acce9179ce54e61b7f5ba4fee9
Coppola-Valentino <espagueti6666@gmail.com>
2026-07-13 04:04:46 -0300

    d2, nothing

--- app/src/main/java/com/example/mobilesvc/Clases/Recordatorio.java
+++ app/src/main/java/com/example/mobilesvc/Clases/Recordatorio.java
@@ -1,4 +1,55 @@
 package com.example.mobilesvc.Clases;
 
-public class Recordatorio {
+import java.io.Serializable;
+
+public class Recordatorio implements Serializable {
+    private int IDRec, UserID, MedicamentoID, cantidad, intervalo;
+    public Recordatorio() {}
+    public Recordatorio(int IDRec, int UserID, int MedicamentoID, int cantidad, int intervalo) {
+        this.IDRec = IDRec;
+        this.UserID = UserID;
+        this.MedicamentoID = MedicamentoID;
+        this.cantidad = cantidad;
+        this.intervalo = intervalo;
+
+    }
+    public int getIDRec() {
+        return IDRec;
+    }
+    public void setIDRec(int IDRec) {
+        this.IDRec = IDRec;
+    }
+    public int getUserID() {
+        return UserID;
+    }
+    public void setUserID(int UserID) {
+        this.UserID = UserID;
+    }
+    public int getMedicamentoID() {
+        return MedicamentoID;
+    }
+    public void setMedicamentoID(int MedicamentoID) {
+        this.MedicamentoID = MedicamentoID;
+    }
+    public int getCantidad() {
+        return cantidad;
+    }
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+    public int getIntervalo() {
+        return intervalo;
+    }
+    public void setIntervalo(int intervalo) {
+        this.intervalo = intervalo;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Recordatorio{" +
+                "cantidad='" + cantidad + '\'' +
+                "intervalo='" + intervalo + '\'' +
+                '}';
+    }
 }
--- app/src/main/java/com/example/mobilesvc/Clases/Usuario.java
+++ app/src/main/java/com/example/mobilesvc/Clases/Usuario.java
@@ -1,4 +1,79 @@
 package com.example.mobilesvc.Clases;
 
-public class Usuario {
+import java.io.Serializable;
+
+public class Usuario implements Serializable {
+    private int IDUser, telefono, edad;
+    private String nombre, rol, password, direccion, genero;
+    public Usuario() {}
+    public Usuario(int IDUser, int telefono, int edad, String nombre, String rol, String password, String direccion, String genero) {
+        this.IDUser = IDUser;
+        this.telefono = telefono;
+        this.edad = edad;
+        this.nombre = nombre;
+        this.rol = rol;
+        this.password = password;
+        this.direccion = direccion;
+        this.genero = genero;
+    }
+    public int getIDUser() {
+        return IDUser;
+    }
+    public void setIDUser(int IDUser) {
+        this.IDUser = IDUser;
+    }
+    public int getTelefono() {
+        return telefono;
+    }
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+    public int getEdad() {
+        return edad;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getRol() {
+        return rol;
+    }
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getDireccion() {
+        return direccion;
+    }
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    public String getGenero() {
+        return genero;
+    }
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "Rol='" + rol + '\'' +
+                "Nombre='" + nombre + '\'' +
+                "Edad='" + edad + '\'' +
+                "Telefono='" + telefono + '\'' +
+                "Genero='" + genero + '\'' +
+                "Direccion='" + direccion + '\'' +
+                '}';
+    }
 }
